9f065bd65f5b94dda39888bd1a57ed03d589246e
jh4n23 <jh4n23@soton.ac.uk>
2026-08-06 16:36:20 +0100

    undo accidental deletion of base_classifier.py

---
+++ classes/base_classifier.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from utils.cnn import CNN
+from torch.utils.data import DataLoader
+from utils.csec_tester import CsecTester
+from art.estimators.classification import PyTorchClassifier
+
+class BaseClassifier():
+    def __init__(self):
+        self.model = CNN(in_channels=1, input_size=28, num_classes=10)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+
+    def train(self, train_loader: DataLoader, num_epochs: int, batch_size: int):
+        criterion = nn.CrossEntropyLoss()
+        for epoch in range(num_epochs):
+            for images, labels in train_loader:
+                self.optimizer.zero_grad
+                logits = self.model(images)
+                loss = criterion(logits, labels)
+
+                loss.backward()
+                self.optimizer.step()
+
+            print(
+                f"Epoch: {epoch + 1}, "
+                f"Loss: {loss.item():.4f}"
+            )
+
+    def evaluate(self, test_data):
+        classifier = PyTorchClassifier(
+            model=self.model,
+            loss=nn.CrossEntropyLoss(),
+            optimizer=self.optimizer,
+            input_shape=(1, 28, 28),
+            nb_classes=10,
+        )
+
+        x_test = torch.stack([img for img, _ in test_data]).numpy()
+        y_test = torch.tensor([label for _, label in test_data]).numpy()
+
+        preds = classifier.predict(x_test)
+        accuracy = np.sum(np.argmax(preds, axis=1) == y_test) / len(y_test)
+        print(f"Benign accuracy: {accuracy * 100:.1f}%")
+
+        tester = CsecTester(model=classifier)
+        adv_accuracy = tester.run(x_test, y_test, 0.1)
+        print(f"Constraint security: {adv_accuracy * 100:.1f}%")
